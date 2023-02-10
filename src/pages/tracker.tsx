e120f5b7e040d50501cc5f299adf953102267fee
daltonben <dalton.bennyhoff@mykenzie.snhu.edu>
2023-02-10 01:14:17 -0600

    Protected route middleware

--- src/pages/tracker.tsx
+++ src/pages/tracker.tsx
@@ -1,12 +1,35 @@
-import { NextPage } from 'next';
+import { GetServerSidePropsContext } from 'next';
+import { Session } from 'next-auth';
+import { useSession } from 'next-auth/react';
 import CreateJobApp from '../components/createJobApp';
+import { getServerAuthSession } from '../server/auth';
 
-const Tracker: NextPage = () => {
+const Tracker = ({ session }: { session: Session }) => {
+  const { data } = useSession();
   return (
     <div>
+      <h1>{data?.user.name}</h1>
       <CreateJobApp />
     </div>
   );
+};
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
 };
 
 export default Tracker;
