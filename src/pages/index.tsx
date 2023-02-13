8205822769c655f6004ef3bf75729e76c6595325
daltonben <dalton.bennyhoff@mykenzie.snhu.edu>
2023-02-13 06:13:21 -0600

    Landing page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,25 @@
-import { Box, Button, Center, Heading, Text } from '@chakra-ui/react';
 import { type NextPage } from 'next';
 import { signIn } from 'next-auth/react';
 
 const Home: NextPage = () => {
   return (
-    <Box w="65%" m="auto" mt={12}>
-      <h1 className="font-bold underline">
-        Tailwind is so much better its not funny
-      </h1>
-      <Center>
-        <Heading size="3xl">On the job hunt?</Heading>
-      </Center>
-      <Center>
-        <Heading size="3xl" mt={8}>
-          Track all your job applications in one spot.
-        </Heading>
-      </Center>
-      <Center>
-        <Text mt={8} fontSize="2xl" fontWeight="medium">
-          JAT is a tool to help you view and manage all of your job
-          applications.
-        </Text>
-      </Center>
-      <Center>
-        <Button
-          size="lg"
-          bg="orange.500"
-          w="400"
-          mt={8}
+    <div className="mt-12">
+      <div className="flex flex-col text-center font-bold text-6xl">
+        <h1 className="text-6xl">On the job hunt?</h1>
+        <h1>Track all your job applications in one place.</h1>
+      </div>
+      <div className="flex flex-col align-middle items-center">
+        <p className="font-medium text-2xl my-4">
+          JAT is a tool to help view and manage all of your job applications.
+        </p>
+        <button
           onClick={() => signIn('discord', { callbackUrl: '/tracker' })}
+          className="bg-orange-500 p-3 w-20 rounded-lg font-bold hover:bg-orange-600"
         >
-          Sign In
-        </Button>
-      </Center>
-    </Box>
+          Sign in
+        </button>
+      </div>
+    </div>
   );
 };
 
