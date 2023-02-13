6989eef2934a909fd5403c4f31e3bbb785f8545b
daltonben <dalton.bennyhoff@mykenzie.snhu.edu>
2023-02-13 06:20:32 -0600

    Header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,20 @@
-import { Box, Flex, Heading, Spacer } from '@chakra-ui/react';
-import ToggleColorMode from './ToggleColorMode';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Header() {
+  const { data: sessionData } = useSession();
   return (
-    <Flex width="65%" margin="auto" mt={10}>
-      <Box>
-        <Heading>JAT</Heading>
-      </Box>
-      <Spacer />
-      <Box>
-        <ToggleColorMode />
-      </Box>
-    </Flex>
+    <div className="flex justify-between">
+      <p className="font-bold text-4xl">JAT</p>
+      {sessionData ? (
+        <button
+          className="bg-orange-500 p-2 w-22 rounded-lg font-bold hover:bg-orange-600"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
