9ee01fe2c45f40c8b7f7aeff766ea6deeed13d98
daltkb <daltkben@gmail.com>
2023-06-03 02:56:21 -0500

    Clean repo

--- src/components/HeroImage.tsx
+++
@@ -1,9 +0,0 @@
-export default function HeroImage() {
-  return (
-    <div className="shadow-2xl shadow-orange-700">
-      <div className="rounded-lg border border-orange-500 bg-white bg-opacity-[0.01] mt-6">
-        <img className="mt-[0.3rem]" src="/JAT-Tracker.png" alt="hero image" />
-      </div>
-    </div>
-  );
-}
