ad9ac0a75fb2ef41fdcaf77357b99951658cc19f
daltkb <daltkben@gmail.com>
2023-06-26 19:37:43 -0500

    Added applied date to job application

--- src/components/AddApplicationForm.tsx
+++ src/components/AddApplicationForm.tsx
@@ -1,63 +1,84 @@
-import { Box, Button, Flex, TextInput, Textarea } from "@mantine/core";
-import { useZodForm } from "~/hooks/useZodForm";
-import { z } from "zod";
-import { api } from "~/utils/api";
+import { Box, Button, Flex, TextInput, Textarea } from '@mantine/core';
+import { DateInput } from '@mantine/dates';
+import { useZodForm } from '~/hooks/useZodForm';
+import { z } from 'zod';
+import { api } from '~/utils/api';
+import { useState } from 'react';
 
 const schema = z.object({
-  companyName: z.string(),
-  jobTitle: z.string(),
-  status: z.string(),
-  note: z.string(),
+    companyName: z.string(),
+    jobTitle: z.string(),
+    appliedDate: z.date(),
+    status: z.string(),
+    note: z.string(),
 });
 
 export default function AddApplicationForm() {
-  const utils = api.useContext();
+    const utils = api.useContext();
 
-  const { mutateAsync } = api.jobApplication.create.useMutation({
-    onSuccess() {
-      utils.jobApplication.getAll.invalidate();
-    },
-  });
+    const [appliedDate, setAppliedDate] = useState<Date>();
 
-  const { register, handleSubmit, reset } = useZodForm({
-    schema,
-  });
+    const { mutateAsync } = api.jobApplication.create.useMutation({
+        onSuccess() {
+            utils.jobApplication.getAll.invalidate();
+        },
+    });
 
-  function handleAddApplication(data: z.infer<typeof schema>) {
-    console.log(data);
-    console.log(data.companyName);
-    mutateAsync(data);
-    reset();
-  }
+    const { register, handleSubmit, reset } = useZodForm({
+        schema,
+    });
 
-  return (
-    <Box>
-      <form onSubmit={handleSubmit((data) => handleAddApplication(data))}>
-        <Flex direction="column" gap="sm">
-          <TextInput
-            label="Company"
-            required
-            placeholder="Company"
-            {...register("companyName")}
-          />
-          <TextInput
-            label="Title"
-            required
-            placeholder="Title"
-            {...register("jobTitle")}
-          />
-          <TextInput
-            label="Status"
-            required
-            placeholder="Status"
-            {...register("status")}
-          />
-          <Textarea label="Notes" placeholder="Notes" {...register("note")} />
-          <Button type="submit" color="orange.7">
-            Create
-          </Button>
-        </Flex>
-      </form>
-    </Box>
-  );
+    function onDateChange(date: Date) {
+        setAppliedDate(date);
+    }
+
+    function handleAddApplication(data: z.infer<typeof schema>) {
+        console.log(data);
+        mutateAsync(data);
+        reset();
+    }
+
+    return (
+        <Box>
+            <form onSubmit={handleSubmit((data) => handleAddApplication(data))}>
+                <Flex direction="column" gap="sm">
+                    <TextInput
+                        label="Company"
+                        required
+                        placeholder="Company"
+                        {...register('companyName')}
+                    />
+                    <TextInput
+                        label="Title"
+                        required
+                        placeholder="Title"
+                        {...register('jobTitle')}
+                    />
+                    <DateInput
+                        label="Applied Date"
+                        type="default"
+                        maxDate={new Date()}
+                        placeholder="Date"
+                        value={appliedDate}
+                        {...register('appliedDate')}
+                        onChange={(value) => onDateChange(value)}
+                    />
+                    <TextInput
+                        label="Status"
+                        required
+                        placeholder="Status"
+                        {...register('status')}
+                    />
+                    <Textarea
+                        label="Notes"
+                        placeholder="Notes"
+                        {...register('note')}
+                    />
+                    <Button type="submit" color="orange.7">
+                        Create
+                    </Button>
+                </Flex>
+            </form>
+        </Box>
+    );
 }
--- src/server/api/routers/jobApplication.ts
+++ src/server/api/routers/jobApplication.ts
@@ -1,43 +1,45 @@
-import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure } from '../trpc';
 
 export const jobApplicationRouter = createTRPCRouter({
-  create: protectedProcedure
-    .input(
-      z.object({
-        companyName: z.string(),
-        jobTitle: z.string(),
-        status: z.string(),
-        note: z.string(),
-      })
-    )
-    .mutation(({ input, ctx }) => {
-      const { prisma, session } = ctx;
-      const { companyName, jobTitle, status, note } = input;
-      const userId = session.user.id;
-      return prisma.jobApplication.create({
-        data: {
-          company_name: companyName,
-          job_title: jobTitle,
-          status: status,
-          note: note,
-          author: {
-            connect: {
-              id: userId,
-            },
-          },
-        },
-      });
-    }),
-  getAll: protectedProcedure
-    .input(z.object({ userId: z.string() }))
-    .query(({ input, ctx }) => {
-      const { prisma } = ctx;
-      const { userId } = input;
-      return prisma.jobApplication.findMany({
-        where: {
-          authorId: userId,
-        },
-      });
-    }),
+    create: protectedProcedure
+        .input(
+            z.object({
+                companyName: z.string(),
+                jobTitle: z.string(),
+                appliedDate: z.date(),
+                status: z.string(),
+                note: z.string(),
+            })
+        )
+        .mutation(({ input, ctx }) => {
+            const { prisma, session } = ctx;
+            const { companyName, jobTitle, appliedDate, status, note } = input;
+            const userId = session.user.id;
+            return prisma.jobApplication.create({
+                data: {
+                    company_name: companyName,
+                    job_title: jobTitle,
+                    applied_date: appliedDate,
+                    status: status,
+                    note: note,
+                    author: {
+                        connect: {
+                            id: userId,
+                        },
+                    },
+                },
+            });
+        }),
+    getAll: protectedProcedure
+        .input(z.object({ userId: z.string() }))
+        .query(({ input, ctx }) => {
+            const { prisma } = ctx;
+            const { userId } = input;
+            return prisma.jobApplication.findMany({
+                where: {
+                    authorId: userId,
+                },
+            });
+        }),
 });
