d4b84c3d1044ec53985e678e503473ea422ab9a6
daltkb <daltkben@gmail.com>
2023-06-28 18:33:31 -0500

    Job application form

--- src/components/AddApplicationForm.tsx
+++ src/components/AddApplicationForm.tsx
@@ -1,40 +1,166 @@
-import { useZodForm } from '~/hooks/useZodForm';
-import { z } from 'zod';
-import { api } from '~/utils/api';
-import { useState } from 'react';
+import * as z from "zod";
+import { api } from "~/utils/api";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Textarea } from "./ui/textarea";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "~/lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "./ui/calendar";
 
 const schema = z.object({
-    companyName: z.string(),
-    jobTitle: z.string(),
-    appliedDate: z.date(),
-    status: z.string(),
-    note: z.string(),
+  companyName: z.string(),
+  jobTitle: z.string(),
+  appliedDate: z.date(),
+  status: z.string(),
+  note: z.string(),
 });
 
 export default function AddApplicationForm() {
-    const utils = api.useContext();
+  const utils = api.useContext();
 
-    const [appliedDate, setAppliedDate] = useState<Date>();
+  const { mutateAsync } = api.jobApplication.create.useMutation({
+    onSuccess() {
+      utils.jobApplication.getAll.invalidate();
+    },
+  });
 
-    const { mutateAsync } = api.jobApplication.create.useMutation({
-        onSuccess() {
-            utils.jobApplication.getAll.invalidate();
-        },
-    });
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
 
-    const { register, handleSubmit, reset } = useZodForm({
-        schema,
-    });
+  function handleAddApplication(data: z.infer<typeof schema>) {
+    console.log(data);
+    mutateAsync(data);
+  }
 
-    function onDateChange(date: Date) {
-        setAppliedDate(date);
-    }
-
-    function handleAddApplication(data: z.infer<typeof schema>) {
-        console.log(data);
-        mutateAsync(data);
-        reset();
-    }
-
-    return <div></div>;
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleAddApplication)}
+        className="flex flex-col gap-3"
+      >
+        <FormField
+          control={form.control}
+          name="companyName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Google" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="jobTitle"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Job Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Software Engineer" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="appliedDate"
+          render={({ field }) => (
+            <FormItem className="flex flex-col w-full">
+              <FormLabel>Date Applied</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date: Date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="status"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Status</FormLabel>
+              <FormControl>
+                <Select onValueChange={field.onChange}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Application Status" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Status</SelectLabel>
+                      <SelectItem value="applied">Applied</SelectItem>
+                      <SelectItem value="interviewing">Interviewing</SelectItem>
+                      <SelectItem value="rejected">Rejected</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="note"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Notes</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Additional Info.."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="mt-2" type="submit">
+          Create
+        </Button>
+      </form>
+    </Form>
+  );
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,37 +1,38 @@
-import { GetServerSidePropsContext } from 'next';
-import Head from 'next/head';
-import AddApplicationModal from '~/components/AddApplicationModal';
-import IndexLayout from '~/components/IndexLayout';
-import { getServerAuthSession } from '~/server/auth';
+import { GetServerSidePropsContext } from "next";
+import Head from "next/head";
+import AddApplicationForm from "~/components/AddApplicationForm";
+import AddApplicationModal from "~/components/AddApplicationModal";
+import IndexLayout from "~/components/IndexLayout";
+import { getServerAuthSession } from "~/server/auth";
 
 export default function Dashboard() {
-    return (
-        <>
-            <Head>
-                <title>JAT | Home</title>
-            </Head>
-            <IndexLayout>
-                <div>Dashboard</div>
-                <AddApplicationModal />
-            </IndexLayout>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>JAT | Home</title>
+      </Head>
+      <IndexLayout>
+        <div>Dashboard</div>
+        <AddApplicationForm />
+      </IndexLayout>
+    </>
+  );
 }
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
-    const session = await getServerAuthSession(ctx);
+  const session = await getServerAuthSession(ctx);
 
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        };
-    }
+  if (!session) {
     return {
-        props: {
-            session,
-        },
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
     };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
 };
