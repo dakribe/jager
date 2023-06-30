a8d413f32321de306a34297e389810cf8ecf3be6
daltkb <daltkben@gmail.com>
2023-06-30 14:32:33 -0500

    Add signout dropdown

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,18 +1,30 @@
 import { signOut, useSession } from "next-auth/react";
-import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function User() {
   const { data: sessionData } = useSession();
   const fallbackInitials = sessionData?.user.name?.slice(0, 2);
 
   return (
-    <Button variant="ghost" className="p-6">
-      <Avatar>
-        <AvatarImage src={sessionData?.user.image} />
-        <AvatarFallback>{fallbackInitials}</AvatarFallback>
-      </Avatar>
-      <p className="ml-4 font-medium">{sessionData?.user.name}</p>
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="flex items-center">
+          <Avatar>
+            <AvatarImage src={sessionData?.user.image!} />
+            <AvatarFallback>{fallbackInitials}</AvatarFallback>
+          </Avatar>
+          <p className="ml-4 font-medium">{sessionData?.user.name}</p>
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => signOut()}>Sign out</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
