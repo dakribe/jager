9562d06e224479f4b2b706f7726ae1f2f7cf20ad
dakribe <dakribe94@gmail.com>
2024-02-18 06:48:28 -0600

    reset repo

--- client/src/components/protectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "@/auth";
-import { Navigate } from "react-router-dom";
-
-export function ProtectedRoute({ children }: { children: React.ReactNode }) {
-  const { isLoggedIn } = useAuth();
-
-  return isLoggedIn === true ? children : <Navigate to="/signin" />;
-}
