1e7780860398a90a92451a2ef76410d20021b8b6
dakribe <dakribe94@gmail.com>
2024-02-18 09:14:24 -0600

    Add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -8,9 +8,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import globalcss from "./globals.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: globalcss },
 ];
 
 export default function App() {
