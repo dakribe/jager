a2fb3e6c061205928fb3a7ce2580ccf926e37b38
dakribe <dakribe94@gmail.com>
2024-03-13 14:18:44 -0500

    reset repo

--- app/routes/magic-link.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticator } from "~/utils/auth.server";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  await authenticator.authenticate("TOTP", request, {
-    successRedirect: "/dashboard",
-    failureRedirect: "/login",
-  });
-}
