68fad1c8587022594178bf8305cbe8b540c6358a
dakribe <dakribe94@gmail.com>
2024-05-09 19:33:32 -0500

    add keybind to open dialog

--- src/components/NewApplicationDialog.tsx
+++ src/components/NewApplicationDialog.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewApplicationForm from "./NewApplicationForm";
 import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
 
 export default function NewApplicationDialog() {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "n") {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger className="w-full">Open</DialogTrigger>
