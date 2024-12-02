1e70156be6ffb2cc17c83064e23b2045a7f93e2e
dalton <dakribe94@gmail.com>
2024-12-02 09:13:22 -0600

    toolbar component

---
+++ src/lib/components/toolbar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import House from 'lucide-svelte/icons/house';
+	import ChartNoAxesColumn from 'lucide-svelte/icons/chart-no-axes-column';
+	import Settings from 'lucide-svelte/icons/settings';
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+</script>
+
+<div
+	class="fixed bottom-8 left-1/2 flex -translate-x-1/2 items-center justify-center gap-4 rounded-3xl border border-white/50 bg-zinc-700 px-6 py-2"
+>
+	<Tooltip.Provider>
+		<Tooltip.Root delayDuration={200}>
+			<Tooltip.Trigger>
+				<a href="/home" class="hover:text-zinc-300">
+					<House size="32" />
+				</a>
+			</Tooltip.Trigger>
+			<Tooltip.Content sideOffset={10}>
+				<p>Home</p>
+			</Tooltip.Content>
+		</Tooltip.Root>
+	</Tooltip.Provider>
+	<Tooltip.Provider>
+		<Tooltip.Root delayDuration={200}>
+			<Tooltip.Trigger>
+				<a href="/analytics" class="hover:text-zinc-300">
+					<ChartNoAxesColumn size="32" />
+				</a>
+			</Tooltip.Trigger>
+			<Tooltip.Content sideOffset={10}>
+				<p>Analytics</p>
+			</Tooltip.Content>
+		</Tooltip.Root>
+	</Tooltip.Provider>
+	<Tooltip.Provider>
+		<Tooltip.Root delayDuration={200}>
+			<Tooltip.Trigger>
+				<a href="/settings" class="hover:text-zinc-300">
+					<Settings size="32" />
+				</a>
+			</Tooltip.Trigger>
+			<Tooltip.Content sideOffset={10}>
+				<p>Settings</p>
+			</Tooltip.Content>
+		</Tooltip.Root>
+	</Tooltip.Provider>
+</div>
--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import CustomForm from './custom-form.svelte';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
+	import Toolbar from '@/components/toolbar.svelte';
 
 	const { data, children } = $props();
 </script>
 
-<div class="mx-auto max-w-[80%]">
+<div class="mx-auto mt-12 max-w-[80%]">
 	<Toaster />
 	<CustomForm form={data.form} />
 	{@render children()}
+	<Toolbar />
 </div>
