e99c9033022703891f74371127e06e7735187ca8
dalton <dakribe94@gmail.com>
2024-12-01 18:42:09 -0600

    fix form schema

--- src/routes/(authed)/schema.ts
+++ src/routes/(authed)/schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createApplicationSchema = z.object({
-	title: z.string().min(2).max(10),
+	title: z.string().min(2).max(32),
 	company: z.string().min(2).max(32),
 	status: z.enum(["Applied", "Rejected", "Accepted"]).optional(),
 	appliedDate: z.string().optional(),
