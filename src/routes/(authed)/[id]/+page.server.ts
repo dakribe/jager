d18b385c0d300c256736b47c36e4bb7ef63808a6
dalton <dakribe94@gmail.com>
2024-12-02 09:56:14 -0600

    delete functionality

--- src/routes/(authed)/[id]/+page.server.ts
+++ src/routes/(authed)/[id]/+page.server.ts
@@ -1,8 +1,20 @@
-import { getById } from "@/server/db/job_application";
+import { deleteJobApplication, getById } from "@/server/db/job_application";
 import type { PageServerLoad } from "./$types";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async (event) => {
 	return {
 		application: await getById(event.params.id),
 	};
+};
+
+export const actions: Actions = {
+	delete: async (event) => {
+		if (event.locals.session === null) {
+			return fail(401);
+		}
+
+		await deleteJobApplication(event.params.id!);
+		redirect(302, "/home");
+	},
 };
--- src/routes/(authed)/[id]/+page.svelte
+++ src/routes/(authed)/[id]/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Button from '@/components/ui/button/button.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
 <h1>{data.application.company}</h1>
 <p>{data.application.title}</p>
+
+<form method="POST" action="?/delete">
+	<Button type="submit" variant="destructive">Delete</Button>
+</form>
