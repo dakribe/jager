bf623ed260626d13a1b11bb0910c0f9f04c2d192
dakribe <dakribe94@gmail.com>
2025-12-31 15:09:59 -0600

    add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="font-medium">Hello World</div>
+    <div>
+      <Button>Hello World</Button>
     </div>
   );
 }
+
